b47e1edb24e784e763514f1bc8fee0cbf82deeb4
Justin Gondron <jgondron@nd.edu>
2018-09-06 16:18:14 -0400

    Image viewer build pipeline
    - Separated deploy.sh into install, prebuild, build, and postbuild phases to better support running locally and in code build
    - Changed it to do a final copy into the /dist directory. This will get copied out to the deployed service

---
+++ build/post_build.sh
@@ -0,0 +1,3 @@
+# copy the uv embeded file so that it is an index file for a webserver to pickup
+cp universalviewer/examples/uv/uv.html universalviewer/examples/uv/index.html
+cp -r universalviewer/examples/uv/* dist/
