585e556ba1a4fc69923bf38666fb3fb25177097c
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-09-13 16:17:26 -0400

    Move the files

--- build/post_build.sh
+++ build/post_build.sh
@@ -1,5 +1,5 @@
 # copy the uv embeded file so that it is an index file for a webserver to pickup
-cp build/index.html universalviewer/examples/uv/index.html
-cp build/config.json universalviewer/examples/uv/
+cp src/index.html universalviewer/examples/uv/index.html
+cp src/config.json universalviewer/examples/uv/
 
 cp -r universalviewer/examples/uv/* dist/
