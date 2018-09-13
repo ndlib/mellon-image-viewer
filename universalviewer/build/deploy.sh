9e152570b93620edcedf6c50528d907d7fa823f6
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-09-13 09:59:03 -0400

    Add checkout step.  And new bucket

--- universalviewer/build/deploy.sh
+++ universalviewer/build/deploy.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-webcomponentstackname="mellon-image-webcomponent-justin"
+webcomponentstackname="mellon-image-webcomponent-dev"
 componentbucketname=$( aws cloudformation describe-stacks --region us-west-2 --stack-name $webcomponentstackname --query 'Stacks[0].Outputs[?OutputKey==`BucketName`].OutputValue' --output text )
 universalviewerbranch=master
 
-
-pushd .
-
+# cleanup if it exists
 rm -rf universalviewer
+
+# get uv
 git clone https://github.com/UniversalViewer/universalviewer.git
+git checkout $universalviewerbranch
 
 cd universalviewer
 
+#install uv mods
 npm install -g grunt
-
-# Install the modules
 npm install
 
 # build the dist
 grunt build --dist
 
-# copy the uv embeded file so that it is at the index
+# copy the uv embeded file so that it is an index file for a webserver to pickup
 cp examples/uv/uv.html examples/uv/index.html
 
 # push to S3
 aws s3 sync --region us-west-2 --delete examples/uv s3://$componentbucketname/universalviewer
-
-popd
