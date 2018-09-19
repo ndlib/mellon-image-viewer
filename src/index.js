d4d5df632dc470a52eccb8da290e8047fb2cf6b5
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-09-19 09:28:35 -0400

    Merge the projects

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.js";
+import React, { Component} from "react";
+import PropTypes from 'prop-types';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById("root")
-);
+const universalViewerUrl = "https://dptsn6y11mgr6.cloudfront.net/universalviewer/index.html"
+class ImageViewer extends Component{
+  render(){
+    return(
+      <iframe width={this.props.width} height={this.props.height} src={universalViewerUrl + "#?manifest=" + this.props.manifest + "&c=" + this.props.c + "&m=" + this.props.m + "&s=" + this.props.s + "&cv=" + this.props.cv + "&xywh=" + this.props.xywh}></iframe>
+    );
+  }
+}
+export default ImageViewer;
+
+ImageViewer.propTypes = {
+  width: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  height: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.number
+  ]),
+  manifest: PropTypes.string,
+  c: PropTypes.string,
+  m: PropTypes.string,
+  s: PropTypes.string,
+  cv: PropTypes.string,
+  xywh: PropTypes.string,
+}
+
+ImageViewer.defaultProps = {
+  width: "800px",
+  height: "600px",
+  manifest: "https://s3.amazonaws.com/mellon-data-broker-publicbucket-wtci53auglzt/manifest_prod.json",
+  c: "",
+  m: "",
+  s: "",
+  cv: "",
+  xywh: "",
+}
