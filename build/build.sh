90bb064664455879f7f3c3325605cef305dc6f7d
Justin Gondron <jgondron@nd.edu>
2018-10-08 14:26:02 -0400

    Force a failure to test BuildFailureEventRule notification

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
-
+echo "Force a failure to test BuildFailureEventRule notification"
+exit 1;
 ./build/universalviewer/build.sh || { echo "Universal Viewer Build Failed"; exit 1; }
 
 ./build/react/build.sh || { echo "React Build Failed"; exit 1; }
