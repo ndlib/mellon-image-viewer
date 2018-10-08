87c12fd9816c84ccc52c671c8763d1c74ce50d83
Justin Gondron <jgondron@nd.edu>
2018-10-08 15:46:01 -0400

    Remove forced fail

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,4 @@
-echo "Force a failure to test BuildFailureEventRule notification"
-exit 1;
+
 ./build/universalviewer/build.sh || { echo "Universal Viewer Build Failed"; exit 1; }
 
 ./build/react/build.sh || { echo "React Build Failed"; exit 1; }
