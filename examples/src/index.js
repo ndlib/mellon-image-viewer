572bbdfbba990db3535a1d25f9bdc50e48254528
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-11-01 13:04:33 -0400

    Update the viewer to read the manifest from the url.

--- examples/src/index.js
+++ examples/src/index.js
@@ -2,7 +2,25 @@
 import React from 'react';
 import { render} from 'react-dom';
 import ImageViewer from '../../src';
+
+// read the url param so that we can set a url in the params
+let urlParams;
+(window.onpopstate = function () {
+    let match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})()
+
 const App = () => (
-    <ImageViewer />
+  <ImageViewer manifest={urlParams['manifest']} />
 );
+
+
+
 render(<App />, document.getElementById("root"));
