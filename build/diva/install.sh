30ea4805eabc0e44dee84d7ce3258fad0809080e
jmille79 <jmille79@nd.edu>
2020-01-10 13:06:24 -0500

    remove unneeded lines

--- build/diva/install.sh
+++ build/diva/install.sh
@@ -1,14 +1,11 @@
 echo "---- INSTALL Diva  ----"
 
-divabranch=master
-
 # cleanup if it exists
 rm -rf diva
 
 # get diva
 git clone --branch v6.0.2 git://github.com/DDMAL/diva.js.git divajs || { echo "Diva git clone failed" ;exit 1; }
 cd divajs
-git checkout $divabranch
 
 #install diva mods
 npm install || { echo "Diva Npm install failed" ;exit 1; }
