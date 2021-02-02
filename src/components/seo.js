3fddce4e5ea1129560879f43d57926790be86676
Jon Hartzler <jon.hartzler@gmail.com>
2021-02-02 10:26:51 -0500

    add tests back in.

--- src/components/seo.js
+++ src/components/seo.js
@@ -33,7 +33,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang,
       }}
-      title={defaultTitle}
+      title={title}
       titleTemplate={null}
       meta={[
         {
