e730727afec42bbdb029448b660d35aa64a52e99
jmille79 <38439231+jmille79@users.noreply.github.com>
2021-04-19 12:27:20 -0400

    add download plugin

--- src/pages/marble.js
+++ src/pages/marble.js
@@ -3,6 +3,7 @@ import { miradorImageToolsPlugin } from 'mirador-image-tools/es/'
 import PropTypes from 'prop-types'
 import Layout from '../components/layout'
 import MiradorRenderer from '../components/miradorrenderer'
+import { miradorDownloadPlugin, MiradorDownloadDialogPlugin } from '../components/miradordlplugin'
 import Seo from '../components/seo'
 
 // eslint-disable-next-line complexity
@@ -16,7 +17,7 @@ const MarbleViewerPage = ({ location }) => {
         description={`View IIIF manifests by adding the manifest url as a query string parameter to this page`}
         noIndex
       />
-      <MiradorRenderer  id="marble-viewer" location={location} plugins={[...miradorImageToolsPlugin]} themeColor="#blue" />
+      <MiradorRenderer  id="marble-viewer" location={location} plugins={[...miradorImageToolsPlugin, miradorDownloadPlugin, MiradorDownloadDialogPlugin]} themeColor="#blue" />
     </Layout>
   )
 }
